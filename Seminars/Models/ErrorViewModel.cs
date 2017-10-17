c710b177a867360eb4054c54d2a15245552c9f5c
Steve Mulholland <smulholland2@gmail.com>
2017-10-17 11:13:43 -0700

    Add project files.

---
+++ Seminars/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seminars.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
